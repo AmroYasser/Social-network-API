628794607c4150c01f9a79d3b7f7ac0c03057e02
AmroYasser <3mryasser97@gmail.com>
2023-01-21 18:11:57 +0200

    add registeration test

--- account/tests.py
+++ account/tests.py
@@ -2,8 +2,8 @@ from rest_framework.test import APITestCase
 from rest_framework import status
 
 
-# class RegisterationTestCase(APITestCase):
-#     def test_registeration(self):
-#         data = {"email": "test@gmail.com", "name": "test_user", "password": "pass"}
-#         response = self.client.post("/account/register/", data)
-#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+class RegisterationTestCase(APITestCase):
+    def test_registeration(self):
+        data = {"email": "test@gmail.com", "name": "test_user", "password": "pass"}
+        response = self.client.post("/account/register/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
